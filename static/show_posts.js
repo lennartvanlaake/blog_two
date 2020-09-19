b1c68b11474efd304ce309c122a90089f9f1412f
Your Name <you@example.com>
2020-09-19 13:07:56 +0200

    infinite scroll works both ways

--- static/show_posts.js
+++ static/show_posts.js
@@ -3,27 +3,40 @@ const backward="backward"
 const pageSize=5
 const converter = new showdown.Converter()
 let lastPost=0
+let firstPostId=999999
 let rendering=false
 
 function appendPosts(start) {
-  if (!rendering) {
-    rendering = true;  
-    loadPosts(start, "forward", appendPostsCallback);
-  }
+  loadPosts(start, "forward", appendPostsCallback);
+}
+
+function prependPosts() {
+  console.log("firstpost: " + (firstPostId - 1));
+  loadPosts(firstPostId - 1, "backward", prependPostsCallback);
 }
 
 function loadPosts(start, direction, callback) {
   $.getJSON("/api/posts?start=" + start + "&size=" + pageSize + "&direction=" + direction, callback)
+}
+function prependPostsCallback(data) {
+  data.forEach(post => {
+    postContainer.prepend(renderPost(post));
+    renderMap("map" + post.id, post.longditude, post.latitude);
+  })
 }
 function appendPostsCallback(data) {
   data.forEach(post => {
     postContainer.append(renderPost(post));
     renderMap("map" + post.id, post.longditude, post.latitude);
     lastPost=post.id;
+    console.log("lastpost: " + lastPost);
   })
-  rendering = false;
 }
 function renderPost(post) {
+  if (post.id < firstPostId) {
+    firstPostId = post.id;
+    console.log("firstpost: " + firstPostId);
+  }
   var postMap = document.createElement("DIV");
   postMap.id = "map" + post.id
   postMap.className += "map"
@@ -69,11 +82,23 @@ function renderMap(elementId, longditude, latitude) {
 
 
 window.onscroll = function(ev) {
+    if (rendering) {
+      return
+    }
     if ((window.innerHeight + window.pageYOffset) >= document.body.scrollHeight) {
-      console.log("bottom")
-      appendPosts(lastPost + 1)
+      console.log("bottom");
+      rendering = true;
+      console.log("rendering: " + rendering)
+      appendPosts(lastPost + 1);
+      rendering = false;
+      console.log("rendering: " + rendering)
+
     }
     if (window.pageYOffset == 0) { 
       console.log("top");
+      rendering = true;
+      prependPosts();
+      rendering = false;
+
     }
 };
